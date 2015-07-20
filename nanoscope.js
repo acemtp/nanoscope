dabc4f160a518a6b8c26c04a4cbe83cf3e836348
Vianney Lecroart <acemtp@gmail.com>
2015-07-20 13:09:48 +0200

    initial version

--- nanoscope.js
+++ nanoscope.js
@@ -1,23 +1,72 @@
+
+Posts = new Mongo.Collection('posts');
+Posts.allow({
+  update: function(userId, post) {return true;},
+  remove: function(userId, post) { return userId},
+});
+
+Meteor.methods({
+  post: function(title, url) {
+    var A = Meteor.user();
+    var post = {
+        userId: A && A._id,
+        author: A && A.emails[0].address,
+        title: title,
+        Url: url
+      };
+
+    Posts.insert(post);
+        },
+upvote: function(postId) {
+    var user = Meteor.user();
+    if (!user) return false;
+
+    Posts.update({
+      _id: postId,
+      upvoters: {$ne: user._id}
+    }, {
+      $addToSet: {upvoters: user._id},
+      $inc: {votes: 1}
+    });
+  }
+});
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Template.postItem.helpers({
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (!_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
+  },
+});
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.posts.helpers({
+    posts: function () {
+      return Posts.find();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.postSubmit.events({
+    'submit form': function(e) {
+      e.preventDefault();
+
+      var post = {
+        url: $(e.target).find('[name=url]').val(),
+        title: $(e.target).find('[name=title]').val(),
+      }
+
+      Meteor.call('post', post.url, post.title);
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Template.postItem.events({
+  'click .upvotable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
+});
+
 }
